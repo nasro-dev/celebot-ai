7db4af5e67fecef5a133d13bc75c636ec43024b5
nasro-dev <boukhelkhalnasro@gmail.com>
2023-12-12 23:18:08 +0100

    add categories components + prisma mysql db

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,10 +1,13 @@
 import { SearchInput } from "@/components/search-input";
-
-const RootPage = () => {
+import prismadb from "@/lib/prismadb";
+import { Categories } from "@/components/categories";
+const RootPage = async () => {
+    const categories = await prismadb.category.findMany()
     return ( 
-    <div className="h-full p-4 space-y-2"> 
-      <SearchInput/>   
-    </div>
+      <div className="h-full p-4 space-y-2"> 
+        <SearchInput/>   
+        <Categories data={categories}/>
+      </div>
       );
 }
  
