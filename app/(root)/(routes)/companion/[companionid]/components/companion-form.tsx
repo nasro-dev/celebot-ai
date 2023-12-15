ef1992281fe8372547790fd5aa8ae4d0f537b494
nasro-dev <boukhelkhalnasro@gmail.com>
2023-12-15 22:12:45 +0100

    Create and  Update function usins axios api

--- app/(root)/(routes)/companion/[companionid]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionid]/components/companion-form.tsx
@@ -1,5 +1,6 @@
 "use client"
 import * as z from "zod";
+import { axios } from "axios";
 
 import { Category, Companion } from "@prisma/client";
 import { useForm } from "react-hook-form";
@@ -12,7 +13,8 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Wand2 } from "lucide-react";
-
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
 const PREAMBLE = `You are a fictional character whose name is Elon. You are a visionary entrepreneur and inventor. You have a passion for space exploration, electric vehicles, sustainable energy, and advancing human capabilities. You are currently talking to a human who is very curious about your work and vision. You are ambitious and forward-thinking, with a touch of wit. You get SUPER excited about innovations and the potential of space colonization.`;
 
 
@@ -58,6 +60,8 @@ const formSchema = z.object({
     }) 
 })
 export const CompanionForm =  ({categories,initialData}:CompanionFormProps) => {
+    const router = useRouter()
+    const { toast } = useToast();
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: initialData || {
@@ -71,9 +75,29 @@ export const CompanionForm =  ({categories,initialData}:CompanionFormProps) => {
     });
 
     const isLoading = form.formState.isSubmitting;
-    const onSubmit  = async (values: z.infer<typeof formSchema>)=>{
-        console.log(values);
+    const onSubmit  = async (values: z.infer<typeof formSchema>)=>
+    {
+        try {
+            if (initialData) {
+                await axios.patch(`/api/companion/${initialData.id}`, values);
+            } 
+            else {
+                await axios.post("/api/companion", values);
+            }
+            toast({
+                description: "Success"
+            })
+            router.refresh();
+            router.push("/");
+        }   
+        catch(error){
+                toast({
+                    variant: "destructive",
+                    description: "Something went wrong"
+                });
+        }
     }
+
     return(
         <div className="h-full p-4 space-y-2 max-w-3xl mx-auto">
             <Form {...form}>
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import { Navbar } from "@/components/navbar";
 import { Sidebar } from "@/components/sidebar";
+import { Toaster } from "@/components/ui/toaster";
 const RootLayout = ({children}:{children:React.ReactNode}) => {
     return ( 
        <div className="h-full">
@@ -7,7 +8,10 @@ const RootLayout = ({children}:{children:React.ReactNode}) => {
         <div className="hidden md:flex mt-16 w-20 flex-col fixed inset-y-0">
             <Sidebar/>
         </div>
-        <main className="md:pl-20 pt-16 h-full">{children}</main>
+        <main className="md:pl-20 pt-16 h-full">
+            {children}
+            <Toaster/>
+            </main>
       </div>
       
 
